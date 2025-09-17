5b9b09b233865ef60444761fabbecbfeefcf07e0
ProSt4rk <yaso33030@gmail.com>
2025-09-17 08:54:52 +0300

    task 2 done

--- script.js
+++ script.js
@@ -63,6 +63,13 @@ Task:
 1) Make an object used as a "map" (key → value), e.g., course codes → titles.
 2) Iterate over it with for...in and display each key and value.
 */
+const courseTitles = { A101: "Intro", A201: "Data Structures" };
+
+for (const key in courseTitles)
+{
+  console.log(key,": ",courseTitles(key));
+}
+
 
 // =========================================
 // TODO-3: STRING OBJECT — charAt() & length
